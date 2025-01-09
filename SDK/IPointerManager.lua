966b896ae13faf5f38819b5ee785c93ca8132f1e
atom0s <atom0s@live.com>
2025-01-09 02:20:14 -0800

    IPointerManager: Normalized test file.

--- SDK/IPointerManager.lua
+++ SDK/IPointerManager.lua
@@ -37,10 +37,10 @@ end
 --]]
 function test.cleanup()
     -- Cleanup test pointers..
-    local pointerManager = AshitaCore:GetPointerManager();
-    if (pointerManager ~= nil) then
-        pointerManager:Delete('sdktest_pointer_1');
-        pointerManager:Delete('sdktest_pointer_2');
+    local mgr = AshitaCore:GetPointerManager();
+    if (mgr ~= nil) then
+        mgr:Delete('sdktest_pointer_1');
+        mgr:Delete('sdktest_pointer_2');
     end
 end
 
@@ -49,31 +49,26 @@ end
 --]]
 function test.exec()
     -- Validate the manager object..
-    local pointerManager = AshitaCore:GetPointerManager();
-    assert(pointerManager ~= nil, 'GetPointerManager returned an unexpected value.');
+    local mgr = AshitaCore:GetPointerManager();
+    assert(mgr ~= nil, 'GetPointerManager returned an unexpected value.');
 
     -- Test adding a pointer.. (direct value)
-    pointerManager:Add('sdktest_pointer_1', 0x13371337);
-    local v = pointerManager:Get('sdktest_pointer_1');
-    assert(v == 0x13371337, 'Get returned an unexpected value.');
+    mgr:Add('sdktest_pointer_1', 0x13371337);
+    assert(mgr:Get('sdktest_pointer_1') == 0x13371337, 'Get returned an unexpected value.');
 
     -- Test adding a pointer.. (scanned)
-    v = pointerManager:Add('sdktest_pointer_2', 'FFXiMain.dll', '8B560C8B042A8B0485', 9, 0);
-    assert(v == pointerManager:Get('entitymap'), 'Get returned an unexpected value.');
+    assert(mgr:Add('sdktest_pointer_2', 'FFXiMain.dll', '8B560C8B042A8B0485', 9, 0) == mgr:Get('entitymap'), 'Get returned an unexpected value.');
 
     -- Test updating an existing pointer..
-    pointerManager:Add('sdktest_pointer_1', 0xDEADBEEF);
-    v = pointerManager:Get('sdktest_pointer_1');
-    assert(v == 0xDEADBEEF, 'Get returned an unexpected value.');
+    mgr:Add('sdktest_pointer_1', 0xDEADBEEF);
+    assert(mgr:Get('sdktest_pointer_1') == 0xDEADBEEF, 'Get returned an unexpected value.');
 
     -- Test deleting pointers..
-    pointerManager:Delete('sdktest_pointer_1');
-    pointerManager:Delete('sdktest_pointer_2');
+    mgr:Delete('sdktest_pointer_1');
+    mgr:Delete('sdktest_pointer_2');
 
-    v = pointerManager:Get('sdktest_pointer_1');
-    assert(v == 0, 'Get returned an unexpected value.');
-    v = pointerManager:Get('sdktest_pointer_2');
-    assert(v == 0, 'Get returned an unexpected value.');
+    assert(mgr:Get('sdktest_pointer_1') == 0, 'Get returned an unexpected value.');
+    assert(mgr:Get('sdktest_pointer_2') == 0, 'Get returned an unexpected value.');
 end
 
 -- Return the test module table..
